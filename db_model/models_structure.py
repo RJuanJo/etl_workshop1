ea2304f0fef8818ff4c15a3f86dd77f08526f6a6
RJuanJo <juanjorendon6@gmail.com>
2024-03-01 19:53:45 -0500

    Loaded data on Data base

--- db_model/models_structure.py
+++ db_model/models_structure.py
@@ -6,22 +6,19 @@ from sqlalchemy import Column, Integer, String, Date, CheckConstraint
 BASE = declarative_base()
 
 
-class RawApplicant(BASE):
+class Candidates(BASE):
     """This class is to create the table candidates in the database from the csv."""
 
     __tablename__ = "candidates"
 
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(50), nullable=True, default=None)
-    last_name = Column(String(50), nullable=True, default=None)
-    email = Column(String(60), nullable=True, default=None)
-    applicant_date = Column(String(20), nullable=True, default=None)
-    country = Column(String(40), nullable=True, default=None)
-    experience_year = Column(String(40), nullable=True, default=None)
-    seniority = Column(String(40), nullable=True, default=None)
-    technology = Column(String(40), nullable=True, default=None)
-    code_challenge_score = Column(String(40), nullable=True, default=None)
-    technical_interview_score = Column(String(40), nullable=True, default=None)
-
-    def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(60))
+    applicant_date = Column(String(20))
+    country = Column(String(40))
+    experience_year = Column(String(40))
+    seniority = Column(String(40))
+    technology = Column(String(40))
+    code_challenge_score = Column(String(40))
+    technical_interview_score = Column(String(40))

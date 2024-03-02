aa2ede7619eb8b2ff60b004351d4cfb14476fee8
RJuanJo <juanjorendon6@gmail.com>
2024-03-02 16:40:17 -0500

    Uploading EDA

--- db_model/models_structure.py
+++ db_model/models_structure.py
@@ -14,11 +14,33 @@ class Candidates(BASE):
     id = Column(Integer, primary_key=True)
     first_name = Column(String(50))
     last_name = Column(String(50))
-    email = Column(String(60))
+    email = Column(String(100))
     applicant_date = Column(String(20))
-    country = Column(String(40))
+    country = Column(String(100))
     experience_year = Column(String(40))
     seniority = Column(String(40))
     technology = Column(String(40))
     code_challenge_score = Column(String(40))
     technical_interview_score = Column(String(40))
+
+
+class BestCandidates(BASE):
+    """This table is to store the data from the candidates after the ETL process."""
+
+    __tablename__ = "aspirant"
+
+    id = Column(Integer, primary_key=True)
+    applicant_date = Column(Date())
+    code_challenge_score = Column(Integer())
+    country = Column(String(100))
+    email = Column(String(100))
+    experience_year = Column(Integer())
+    first_name = Column(String(50))
+    is_hire = Column(Integer())
+    last_name = Column(String(50))
+    seniority = Column(String(40))
+    technical_interview_score = Column(Integer())
+    technology = Column(String(40))
+    technology_category = Column(String(40))
+
+    __table_args__ = (CheckConstraint("is_hire >= 0 AND is_hire <= 1"),)
